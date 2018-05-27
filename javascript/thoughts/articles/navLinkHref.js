ac445e019f64dcb33c90a26d11983fab53864c3f
Hitasya <33087457+Hitasya@users.noreply.github.com>
2018-05-27 10:37:31 +0400

    manage links for article pages with javascript
    make article-page links independent of the each html, and put in javascript instead so that if sitemap changes in future, much easier to change the links

---
+++ javascript/thoughts/articles/navLinkHref.js
@@ -0,0 +1,10 @@
+document.querySelector('#footer-nav--articles-dir').href = "../articles-directory.html";
+document.querySelector('#footer-nav--home').href = "../../../index.html";
+document.querySelector('#footer-nav--about').href = "../../about/about-me.html";
+
+document.querySelector('#header-nav--about').href = "../../about/about-me.html";
+document.querySelector('#header-nav--home').href = "../../../index.html";
+document.querySelector('#header-nav--articles-dir').href = "../articles-directory.html";
+
+
+
